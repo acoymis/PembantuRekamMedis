207ea2e48c91c6648d37864750f59c789c384a90
acoymis <virgiahmad@gmail.com>
2024-12-22 03:51:26 +0700

    Add files via upload

---
+++ checklistConfig.js
@@ -0,0 +1,9 @@
+// checklistConfig.js
+export const checklistConfig = [
+  { id: 'feverCheckbox', label: 'Fever', text: 'Patient has fever.' },
+  { id: 'headacheCheckbox', label: 'Headache', text: 'Patient complains of headache.' },
+  { id: 'coughCheckbox', label: 'Cough', text: 'Patient reports persistent coughing.' },
+  { id: 'fatigueCheckbox', label: 'Fatigue', text: 'Patient feels fatigued.' },
+  { id: 'nauseaCheckbox', label: 'Nausea', text: 'Patient experiences nausea.' },
+  { id: 'dizzinessCheckbox', label: 'Dizziness', text: 'Patient feels dizzy.' }
+];
